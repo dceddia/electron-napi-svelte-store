b266d0b07122f4ee472d0f399d2aba99f5beb384
Dave Ceddia <dave@daveceddia.com>
2022-02-12 22:57:50 -0500

    Using ThreadsafeFunction doesn't really work
    ThreadsafeFunctions are called with the "Node calling convention" where the
    first argument is the error, or null. This doesn't match the Svelte store
    contract and all Svelte sees is `null`.

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,17 +1,9 @@
-use std::{
-  sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc,
-  },
-  thread::{self, JoinHandle},
-  time::Duration,
-};
-
 use napi::{
   bindgen_prelude::*,
-  threadsafe_function::{ThreadSafeCallContext, ThreadsafeFunction},
-  JsNumber,
+  threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode},
+  CallContext, JsUndefined,
 };
+use std::collections::HashMap;
 
 #[macro_use]
 extern crate napi_derive;
@@ -22,83 +14,79 @@ pub fn sum(a: i32, b: i32) -> i32 {
 }
 
 #[napi]
-pub struct JsRepeater {
-  handle1: Option<JoinHandle<Result<()>>>,
-  handle2: Option<JoinHandle<Result<()>>>,
-  quit: Arc<AtomicBool>,
+pub struct Ticker {
+  value: u32,
+  subscribers: HashMap<u64, ThreadsafeFunction<u32>>,
+  next_subscriber: u64,
 }
 
 #[napi]
-impl JsRepeater {
+impl Ticker {
   #[napi(constructor)]
-  pub fn new(callback1: JsFunction, callback2: JsFunction) -> Result<Self> {
-    // Create a threadsafe function around each callback
-    //
-    // Other things to try:
-    //   - call create_threadsafe_function twice on the same callback (has same effect)
-    //   - only create 1 threadsafe function (doesn't deadlock-panic, but does panic when failing to release)
-    let cb1: ThreadsafeFunction<u32> = callback1.create_threadsafe_function(0, send_update)?;
-    let cb2: ThreadsafeFunction<u32> = callback2.create_threadsafe_function(0, send_update)?;
-
-    let quit = Arc::new(AtomicBool::new(false));
-    let handle1 = spawn_worker("ONE".to_string(), quit.clone(), cb1);
-    let handle2 = spawn_worker("TWO".to_string(), quit.clone(), cb2);
-
-    Ok(JsRepeater {
-      handle1: Some(handle1),
-      handle2: Some(handle2),
-      quit,
-    })
-  }
-}
-
-impl Drop for JsRepeater {
-  fn drop(&mut self) {
-    self.quit.store(true, Ordering::SeqCst);
-
-    if let Some(thread) = self.handle1.take() {
-      println!("joining handle1");
-      let _ = thread.join();
-      println!("joined handle1");
-    }
-
-    if let Some(thread) = self.handle2.take() {
-      println!("joining handle2");
-      let _ = thread.join();
-      println!("joined handle2");
+  pub fn new(value: Option<u32>) -> Ticker {
+    Ticker {
+      value: value.unwrap_or(0),
+      subscribers: HashMap::new(),
+      next_subscriber: 0,
     }
   }
-}
 
-fn send_update(ctx: ThreadSafeCallContext<u32>) -> Result<Vec<JsNumber>> {
-  ctx.env.create_uint32(ctx.value).map(|v| vec![v])
-}
+  #[napi]
+  pub fn increment(&mut self, env: Env) -> Result<()> {
+    self.value += 1;
+    println!("increment! now {}", self.value);
+    self.notify_subscribers(env)
+  }
 
-fn spawn_worker(
-  name: String,
-  should_quit: Arc<AtomicBool>,
-  ts_callback: ThreadsafeFunction<u32>,
-) -> JoinHandle<Result<()>> {
-  thread::spawn(move || {
-    let mut i = 0;
-
-    while !should_quit.load(Ordering::SeqCst) {
-      thread::sleep(Duration::from_millis(3));
-      let status = ts_callback.call(
-        Ok(i),
-        napi::threadsafe_function::ThreadsafeFunctionCallMode::NonBlocking,
-      );
-      //println!("called function {} with {}, {}", name, i, status);
-      i += 1;
+  fn notify_subscribers(&mut self, env: Env) -> Result<()> {
+    for (_, callback) in &self.subscribers {
+      // let args = vec![env.create_double(self.value as f64)?];
+      println!("calling subscriber w/ {}", self.value);
+      // callback.call(None, &args)?;
+      callback.call(Ok(self.value), ThreadsafeFunctionCallMode::Blocking);
     }
 
-    /*
-    println!(
-      "done with loop, callback is aborted? {:?}",
-      ts_callback.aborted()
-    );
-    */
-
+    println!("notify complete");
     Ok(())
-  })
+  }
+
+  /// Implement Svelte's Store Contract, defined as:
+  ///
+  /// store = {
+  ///   subscribe: (
+  ///     subscription: (value: any) => void
+  ///   ) => (() => void), set?: (value: any) => void
+  /// }
+  ///
+  /// - A store must contain a .subscribe method, which must accept as its argument a subscription function.
+  ///   This subscription function must be immediately and synchronously called with the store's current value
+  ///   upon calling .subscribe. All of a store's active subscription functions must later be synchronously
+  ///   called whenever the store's value changes.
+  /// - The .subscribe method MUST return an `unsubscribe` function.
+  ///   Calling an `unsubscribe` function must stop its subscription, and its corresponding subscription
+  ///   function must not be called again by the store.
+  /// - A store may optionally contain a `.set` method, which must accept as its argument a new value
+  ///   for the store, and which synchronously calls all of the store's active subscription functions.
+  ///   Such a store is called a writable store.
+  #[napi]
+  pub fn subscribe(&mut self, env: Env, callback: JsFunction) -> Result<JsFunction> {
+    // Create a threadsafe wrapper
+    let tsfn: ThreadsafeFunction<u32, ErrorStrategy::CalleeHandled> = callback
+      .create_threadsafe_function(0, |ctx| ctx.env.create_uint32(ctx.value).map(|v| vec![v]))?;
+
+    // Call once with the initial value
+    tsfn.call(Ok(self.value), ThreadsafeFunctionCallMode::Blocking);
+
+    // Save the callback in a way that we can call it later, and remove it
+    let key = self.next_subscriber;
+    self.next_subscriber += 1;
+    self.subscribers.insert(key, tsfn);
+
+    let unsubscribe = |ctx: CallContext| -> Result<JsUndefined> {
+      // self.subscribers.remove(&key);
+      ctx.env.get_undefined()
+    };
+
+    env.create_function_from_closure("unsubscribe", unsubscribe)
+  }
 }
