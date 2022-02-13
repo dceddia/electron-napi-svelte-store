572e5e1e4ea6ca579fef83dcc15d2fdaa793c936
Dave Ceddia <dave@daveceddia.com>
2022-02-13 13:30:44 -0500

    Simplify and restyle a little

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,6 +1,5 @@
 <script>
 	import Ticker from "./Ticker.svelte";
-	export let name;
 
 	let showTicker = true;
 
@@ -8,9 +7,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>2 + 2 = {Napi.sum(2, 2)}</p>
-	<button on:click={() => (showTicker = !showTicker)}>Toggle Ticker</button>
+	<h1>Svelte store, built in Rust!</h1>
+	<button on:click={() => (showTicker = !showTicker)}>Toggle Counter</button>
 	<button on:click={() => ($ticker = Math.floor(Math.random() * 1234))}>
 		Set to random number
 	</button>
@@ -22,7 +20,6 @@
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
@@ -30,9 +27,8 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		font-size: 2em;
+		font-weight: 400;
 	}
 
 	@media (min-width: 640px) {
